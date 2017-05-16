cb4e501ab296d3b124fc7a40b242cc069aba2479
NovaPS <novaps@yanex.ru>
2017-05-16 22:54:38 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergey
+ * Date: 16.05.2017
+ * Time: 21:54
+ */
+$array = [1, 2, 3];
+var_dump($array);
+$array[1]++;
+
+$array[1] += 3;
+
+echo $array;
+?>
